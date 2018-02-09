6f4eb39a9394e5a17d0924524edfaedfc9b30d31
contact <contact@onetaxgst.in>
2018-02-09 09:55:13 +0000

    did some configuration changes

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.add_delivery_method :ses,
                                        AWS::SES::Base,
-                                       access_key_id: 'AKIAIIKRMOE66BCAWHMQ',
-                                       secret_access_key: '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',
-                                       server: 'email.us-east-1.amazonaws.com'
+                                       access_key_id: 'your_key',
+                                       secret_access_key: 'your_access_key',
+                                       server: 'your amazon server'
